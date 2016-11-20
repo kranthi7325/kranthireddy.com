d9fc8f4743b4ef0ea19d34c70371ebcb68536224
kranthireddy Gurram <rrkr2312@gmail.com>
2016-11-20 15:37:29 -0800

    edits and added image

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
 	function parallex() {
 		image = document.getElementById('image');
 		ypos = window.pageYOffset;
-		image.style.top = ypos * .7+ 'px';
+		image.style.top = ypos * .8+ 'px';
 	}
 	window.addEventListener('scroll', parallex),false;
